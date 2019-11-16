70cea3d1bbda1bc5c9e211a4b18e4b5fe281eda0
Robin Millette <robin@millette.info>
2019-11-16 13:07:14 -0500

    Clean up.

--- gen-keys/main.js
+++ gen-keys/main.js
@@ -10,29 +10,22 @@ gen()
 .then(({ publicJwk, privateJwk }) => {
   clearInterval(terval)
   elSmall.remove()
-  // document.querySelector("#public-jwk").innerText = JSON.stringify(publicJwk, null, 2)
-  // document.querySelector("#private-jwk").innerText = JSON.stringify(privateJwk, null, 2)
 
   const d1 = document.querySelector("#public-jwk")
-  d1.innerText = ""
-
   const d2 = document.querySelector("#private-jwk")
+
+  d1.innerText = ""
   d2.innerText = ""
 
-
   const blob1 = new window.Blob([JSON.stringify(publicJwk, null, 2)], { type: 'application/json' })
-
   const u1 = URL.createObjectURL(blob1)
-
   const link1 = document.createElement("a")
   link1.href = u1
   link1.download = "public-jwk.json"
   link1.innerText = "Save public-jwk.json"
 
   const blob2 = new window.Blob([JSON.stringify(privateJwk, null, 2)], { type: 'application/json' })
-
   const u2 = URL.createObjectURL(blob2)
-
   const link2 = document.createElement("a")
   link2.href = u2
   link2.download = "private-jwk.json"
