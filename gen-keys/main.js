6a249428cf46a8eed37ede5900b963ccd037f16f
Robin Millette <robin@millette.info>
2019-11-16 13:03:53 -0500

    Progress feedback.

--- gen-keys/main.js
+++ gen-keys/main.js
@@ -1,9 +1,44 @@
 import gen from "./gen-keys"
 
+const elSmall = document.querySelector("h1 > small")
+
+const terval = setInterval(() => {
+  elSmall.innerText = elSmall.innerText + '.'
+}, 333)
+
 gen()
 .then(({ publicJwk, privateJwk }) => {
-  document.querySelector("h1 > small").remove()
-  document.querySelector("#public-jwk").innerText = JSON.stringify(publicJwk, null, 2)
-  document.querySelector("#private-jwk").innerText = JSON.stringify(privateJwk, null, 2)
+  clearInterval(terval)
+  elSmall.remove()
+  // document.querySelector("#public-jwk").innerText = JSON.stringify(publicJwk, null, 2)
+  // document.querySelector("#private-jwk").innerText = JSON.stringify(privateJwk, null, 2)
+
+  const d1 = document.querySelector("#public-jwk")
+  d1.innerText = ""
+
+  const d2 = document.querySelector("#private-jwk")
+  d2.innerText = ""
+
+
+  const blob1 = new window.Blob([JSON.stringify(publicJwk, null, 2)], { type: 'application/json' })
+
+  const u1 = URL.createObjectURL(blob1)
+
+  const link1 = document.createElement("a")
+  link1.href = u1
+  link1.download = "public-jwk.json"
+  link1.innerText = "Save public-jwk.json"
+
+  const blob2 = new window.Blob([JSON.stringify(privateJwk, null, 2)], { type: 'application/json' })
+
+  const u2 = URL.createObjectURL(blob2)
+
+  const link2 = document.createElement("a")
+  link2.href = u2
+  link2.download = "private-jwk.json"
+  link2.innerText = "Save private-jwk.json"
+
+  d1.appendChild(link1)
+  d2.appendChild(link2)
 })
 .catch(console.error)
