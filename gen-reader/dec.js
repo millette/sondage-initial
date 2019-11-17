4cc2cdb070230df64a14f894b3036cd725564c8d
Robin Millette <robin@millette.info>
2019-11-17 13:00:37 -0500

    Cleanup.

--- gen-reader/dec.js
+++ gen-reader/dec.js
@@ -1,31 +1,23 @@
-import privK from "./keypair-private-4096.json";
+import privK from "./private-jwk.json"
 
-const tdec = new TextDecoder("utf-8");
+const tdec = new TextDecoder("utf-8")
+
+const algo = { name: "RSA-OAEP", hash: "SHA-256" }
 
 // FIXME: reference (taken from... mafintosh..?)
-const charCodeAt = c => c.charCodeAt(0);
-const base64ToArrayBuffer = x =>
-  new Uint8Array(
-    atob(x)
-      .split("")
-      .map(charCodeAt)
-  );
+const charCodeAt = (c) => c.charCodeAt(0)
+const base64ToArrayBuffer = (x) => new Uint8Array(atob(x).split("").map(charCodeAt))
 
 const decryptString = (k, s) => {
   try {
-    return window.crypto.subtle
-      .decrypt({ name: "RSA-OAEP" }, k, base64ToArrayBuffer(s))
-      .then(tdec.decode.bind(tdec));
+    return window.crypto.subtle.decrypt(algo, k, base64ToArrayBuffer(s))
+      .then((x) => tdec.decode(x))
   } catch (e) {
-    return Promise.reject(e);
+    return Promise.reject(e)
   }
-};
+}
 
-const makeDecryptor = () =>
-  window.crypto.subtle
-    .importKey("jwk", privK, { name: "RSA-OAEP", hash: "SHA-256" }, false, [
-      "decrypt"
-    ])
-    .then(decer => decryptString.bind(null, decer));
+const makeDecryptor = () => window.crypto.subtle.importKey("jwk", privK, algo, false, ["decrypt"])
+    .then(decer => decryptString.bind(null, decer))
 
-export default makeDecryptor;
+export default makeDecryptor
