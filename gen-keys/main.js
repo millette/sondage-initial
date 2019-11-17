c81cfd45ac519e1d6c92330851acdc1fefbda6f4
Robin Millette <robin@millette.info>
2019-11-17 08:39:20 -0500

    Show error to user.

--- gen-keys/main.js
+++ gen-keys/main.js
@@ -2,36 +2,44 @@ import gen from "./gen-keys"
 
 const elSmall = document.querySelector("h1 > small")
 
-const terval = setInterval(() => {
-  elSmall.innerText = elSmall.innerText + '.'
-}, 333)
+const terval = setInterval(() => elSmall.innerText = elSmall.innerText + '.', 333)
 
-gen()
-.then(({ publicJwk, privateJwk }) => {
-  clearInterval(terval)
-  elSmall.remove()
-
-  const d1 = document.querySelector("#public-jwk")
-  const d2 = document.querySelector("#private-jwk")
-
+const saveLink = (n, k) => {
+  const d1 = document.querySelector(`#${n}`)
   d1.innerText = ""
-  d2.innerText = ""
-
-  const blob1 = new window.Blob([JSON.stringify(publicJwk, null, 2)], { type: 'application/json' })
+  const blob1 = new window.Blob([JSON.stringify(k, null, 2)], { type: 'application/json' })
   const u1 = URL.createObjectURL(blob1)
   const link1 = document.createElement("a")
   link1.href = u1
-  link1.download = "public-jwk.json"
-  link1.innerText = "Save public-jwk.json"
-
-  const blob2 = new window.Blob([JSON.stringify(privateJwk, null, 2)], { type: 'application/json' })
-  const u2 = URL.createObjectURL(blob2)
-  const link2 = document.createElement("a")
-  link2.href = u2
-  link2.download = "private-jwk.json"
-  link2.innerText = "Save private-jwk.json"
-
+  link1.download = `${n}.json`
+  link1.innerText = `Save ${n}.json`
   d1.appendChild(link1)
-  d2.appendChild(link2)
-})
-.catch(console.error)
+}
+
+const doit = ({ publicJwk, privateJwk }) => {
+  clearInterval(terval)
+  elSmall.remove()
+  saveLink("public-jwk", publicJwk)
+  saveLink("private-jwk", privateJwk)
+}
+
+const oups = (e) => {
+  clearInterval(terval)
+  elSmall.remove()
+  const el = document.createElement("div")
+  const p = document.createElement("p")
+  const b = document.createElement("b")
+  b.innerText = e
+  p.appendChild(b)
+
+  const c = document.createElement("h1")
+  c.innerText = "ERROR!"
+  el.appendChild(c)
+
+  el.appendChild(p)
+  document.querySelector("body").appendChild(el)
+}
+
+gen()
+.then(doit)
+.catch(oups)
