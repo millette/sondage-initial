416922c412d2b538ecd0b56b5b45c1c4a44ca60a
Robin Millette <robin@millette.info>
2019-11-17 11:40:59 -0500

    Clean up.

--- gen-survey/enc.js
+++ gen-survey/enc.js
@@ -1,21 +1,20 @@
-import pubK from "./keypair-public-4096.json";
+import pubK from "./public-jwk.json"
 
-const tenc = new TextEncoder();
+const tenc = new TextEncoder()
+
+const algo = { name: "RSA-OAEP", hash: "SHA-256" }
 
 // FIXME: reference (taken from...)
 const arrayBufferToBase64 = ab =>
-  window.btoa(String.fromCharCode(...new Uint8Array(ab)));
+  window.btoa(String.fromCharCode(...new Uint8Array(ab)))
 
 const encryptString = (k, s) =>
-  window.crypto.subtle
-    .encrypt({ name: "RSA-OAEP" }, k, tenc.encode(s))
+    // .encrypt({ name: "RSA-OAEP" }, k, tenc.encode(s))
+  window.crypto.subtle.encrypt(algo, k, tenc.encode(s))
     .then(arrayBufferToBase64);
 
 const makeEncryptor = () =>
-  window.crypto.subtle
-    .importKey("jwk", pubK, { name: "RSA-OAEP", hash: "SHA-256" }, false, [
-      "encrypt"
-    ])
-    .then(encer => encryptString.bind(null, encer));
+  window.crypto.subtle.importKey("jwk", pubK, algo, false, ["encrypt"])
+    .then(encer => encryptString.bind(null, encer))
 
-export default makeEncryptor;
+export default makeEncryptor
