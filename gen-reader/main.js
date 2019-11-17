5c045e3220892987bac41134e075cab623b63885
Robin Millette <robin@millette.info>
2019-11-17 11:40:17 -0500

    Stub.

---
+++ gen-reader/main.js
@@ -0,0 +1,12 @@
+// import dec from "./dec"
+
+/*
+const woot = () => {
+  const str = "U+FoIalg5GxVLnfxIVjb8Grw5eiTN778duceNGBdpwBmum6jb8pMH4k5PPd/jpgqK7/0v7f2UIOji2os9ITxpHp897FKeSX0ZhTgm3Qt5HuCXml4pZbQh/m8XxWhoBdD3oi8Sclaj9jhcnjh9i+oP87semm1hQHw9wSbLdfXZKhMvEdOpbARTK6O7Eof5/96svKnTGZcINLr66YjQDf2BJBlrimqDTYnyatkUYzQ/aDJU930dnCUHMrCgXT4G4JFX17ZlR4eliuRrK89MqsGbgOTK+KAKJ6O3yP6vi6DvNlP/NivIianollqIUPH6N2EmboPpjD0gdfUa85rMy14T0O2hAiSelzc1it2yNdEc7uQp6T/uHcpiv2FwYzlIfVtM8jIbzuYMCLkaN3CjgsuFyE9Wux2ShmoltaZg4fkE++/ioxhMCMegT5qjnmOsrB8y2EJuS1z+PmB9j9oiT+Ldvz+AUZO53Ew/YnzxqPV70qZoijDwwBFLrSyfbBxpAaHrupNMoQK4EcTHSC4WAGXwICODR1hoMpgROZt9PH31rlDl+yXoeFgifRgrAKRvx4bu0OvGLgrJrWDaG11maSVJVue2TWjjCOhi6ThsWDhfHlNdhwGxdBqKE5Gw/7T2qRNhsFwBeiOYRl9ZkY9NXT/8llRH6YbE84FayJJqTXwoOc="
+  return dec().then((x) => x(str))
+}
+
+woot()
+.then(console.log)
+.catch(console.error)
+*/
