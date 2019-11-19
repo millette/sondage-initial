01e55e5db64497edaec0b3afc8d341d01eda431f
Robin Millette <robin@millette.info>
2019-11-18 20:23:00 -0500

    Display available responses list.

--- gen-reader/main.js
+++ gen-reader/main.js
@@ -1,5 +1,47 @@
 import dec from "./dec"
 
+const jsonStoreIo = process.env.JSONSTOREIO
+const elU1 = `https://www.jsonstore.io/${jsonStoreIo}/keys`
+
+const $elResponses = document.querySelector("#responses")
+
+
+
+$elResponses.addEventListener("click", (ev) => {
+  ev.preventDefault()
+  if (!ev.target.dataset.id) return
+  console.log("target", ev.target)
+  console.log("data", ev.target.dataset.id)
+
+},
+)
+fetch(elU1)
+  .then((res) => res.json())
+  .then(({ ok, result }) => {
+    const responses = []
+    for (const id in result) {
+      if (typeof result[id] === "number") responses.push({
+        createdAt: result[id], // new Date(result[id]),
+        id
+      })
+    }
+    return responses
+      .sort(({ createdAt: ac }, { createdAt: bc }) => {
+        if (ac > bc) return -1
+        if (ac < bc) return 1
+      })
+      .map(({ id, createdAt }) => ({ id, createdAt: new Date(createdAt) }))
+      .forEach(({ id, createdAt }) => {
+        const el = document.createElement("li")
+        el.dataset.id = id
+        el.innerText = createdAt
+        $elResponses.appendChild(el)
+      })
+
+
+  })
+  // .then(console.log)
+
 const oups = (e) => {
   const p = document.createElement("p")
   p.innerText = e
@@ -18,9 +60,11 @@ const elSubmit = (dg) => (ev) => {
     .catch(oups)
 }
 
+/*
 dec()
   .then((elDec) => {
     const elForm = document.querySelector("form")
     elForm.addEventListener("submit", elSubmit(elDec))
   })
   .catch(oups)
+*/
