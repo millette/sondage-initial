f760b9a083224a54c280b0baa7f53ddc5be8778e
Robin Millette <robin@millette.info>
2019-11-19 10:47:49 -0500

    Fetch remote response.

--- gen-reader/main.js
+++ gen-reader/main.js
@@ -4,24 +4,37 @@ const jsonStoreIo = process.env.JSONSTOREIO
 const elU1 = `https://www.jsonstore.io/${jsonStoreIo}/keys`
 
 const $elResponses = document.querySelector("#responses")
+const $elResponseId = document.querySelector("#response-id")
+const $elResponse = document.querySelector("#response")
 
+dec()
+  .then((elDec) => {
+    $elResponses.addEventListener("click", (ev) => {
+      ev.preventDefault()
+      const { id } = ev.target.dataset
+      if (!id) return
+      $elResponseId.innerText = id
+      const elU2 = `https://www.jsonstore.io/${jsonStoreIo}/responses/${id}`
+      fetch(elU2)
+        .then((res) => res.json())
+        .then(({ ok, result }) => {
+          if (ok) return elDec(result)
+          throw new Error('Not ok')
+        })
+        .then((xyz) => {
+          $elResponse.innerText = JSON.stringify(JSON.parse(xyz), null, 2)
+        })
+        .catch(console.error)
+    })
+  })
 
-
-$elResponses.addEventListener("click", (ev) => {
-  ev.preventDefault()
-  if (!ev.target.dataset.id) return
-  console.log("target", ev.target)
-  console.log("data", ev.target.dataset.id)
-
-},
-)
 fetch(elU1)
   .then((res) => res.json())
   .then(({ ok, result }) => {
     const responses = []
     for (const id in result) {
       if (typeof result[id] === "number") responses.push({
-        createdAt: result[id], // new Date(result[id]),
+        createdAt: result[id],
         id
       })
     }
@@ -37,34 +50,4 @@ fetch(elU1)
         el.innerText = createdAt
         $elResponses.appendChild(el)
       })
-
-
   })
-  // .then(console.log)
-
-const oups = (e) => {
-  const p = document.createElement("p")
-  p.innerText = e
-  document.querySelector("body").appendChild(p)
-}
-
-const elSubmit = (dg) => (ev) => {
-  ev.preventDefault()
-  dg(new window.FormData(ev.target).get("msg"))
-    .then((bo) => {
-      const j = JSON.parse(bo)
-      const pre = document.createElement("pre")
-      pre.innerText = JSON.stringify(j, null, 2)
-      document.querySelector("body").appendChild(pre)
-    })
-    .catch(oups)
-}
-
-/*
-dec()
-  .then((elDec) => {
-    const elForm = document.querySelector("form")
-    elForm.addEventListener("submit", elSubmit(elDec))
-  })
-  .catch(oups)
-*/
