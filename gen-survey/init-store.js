4c9e016593f3329634155ef464ecb9358a867a8c
Robin Millette <robin@millette.info>
2019-11-19 11:26:16 -0500

    Improve init-store script a bit.

--- gen-survey/init-store.js
+++ gen-survey/init-store.js
@@ -13,11 +13,21 @@ got("https://www.jsonstore.io/get-token", { json: true })
     })
   ]))
   .then(([token, { headers, url, body: { ok, ...rest } }]) => {
-    if (ok) return console.log(ok && token)
-    const err new Error("Problem!")
-    err.url = url
-    err.headers = headers
-    err.more = rest
-    throw err
+    if (!ok) {
+      const err = new Error("Problem!")
+      err.url = url
+      err.headers = headers
+      err.more = rest
+      throw err
+    }
+    console.log(`If you don't already have an \`.env\` file or you don't mind losing its content,
+you can initialize it with the following content:
+
+JSONSTOREIO=${token}
+
+If you loose your JSONSTOREIO token, you will loose access to all the responses you've gathered.
+
+Create a separate JSONSTOREIO token for each survey you want to host.
+`)
   })
   .catch(console.error)
