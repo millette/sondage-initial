81a1907b728147b3d4156d1facc3537b944ddfb6
Robin Millette <robin@millette.info>
2019-11-20 12:26:02 -0500

    Add script to generate reader (with actual survey).

---
+++ gen-reader/oy.js
@@ -0,0 +1,31 @@
+// core
+const { readFile, writeFile } = require('fs').promises
+
+// npm
+// const slug = require("rehype-slug")
+// const toc = require("rehype-toc")
+
+// self
+const main = require("./rehype-main")
+const shared = require("../rehype-shared")
+
+
+const { process } = shared.post(shared.pre({ formConfig: { noForm: true } })
+  // .use(slug)
+  .use(main)
+  // .use(toc, { position: "beforebegin", headings: ["h2"] })
+)
+
+const writer = (c2) => writeFile("questions.html", c2)
+
+const oups = (error) => {
+  if ((error.code === "ENOENT") && error.path) {
+    return console.error(`Did you forget to create ${error.path}? You can use questions-sample.md as a starter.`)
+  }
+  console.error(error)
+}
+
+readFile("../questions.md")
+  .then(process)
+  .then(writer)
+  .catch(oups)
